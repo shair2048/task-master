0e9b7a83a9581d2cd0b444b12108d3184fb35c35
Ken <sonhai.it.2048@gmail.com>
2024-11-02 10:58:02 +0700

    hello world

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
