c9591d6fba71669af2e60a068f395cb36cf1e63a
Ken <sonhai.it.2048@gmail.com>
2024-12-09 12:22:44 +0700

    updated: api file

--- api.ts
+++ api.ts
@@ -1,7 +1,16 @@
 import axios from "axios";
+// import NetInfo from "@react-native-community/netinfo";
+
+// NetInfo.fetch().then((state) => {
+//   if (state.type === "wifi" && state.details && state.details.ipAddress) {
+//     console.log("Địa chỉ IP:", state.details.ipAddress);
+//   } else {
+//     console.log("Không phải kết nối Wi-Fi hoặc không có IP");
+//   }
+// });
 
 const api = axios.create({
-  baseURL: "http://192.168.1.6:3000/api",
+  baseURL: "http://192.168.1.89:3000/api",
   timeout: 5000,
 });
 
